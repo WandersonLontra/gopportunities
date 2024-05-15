02c0a2faef7b99c916856b9addbfa293e3fe52c4
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2024-05-15 11:41:05 -0300

    feat: create logger configs

--- cmd/gopportunities/main.go
+++ cmd/gopportunities/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"github.com/WandersonLontra/gopportunities/configs"
 	"github.com/WandersonLontra/gopportunities/router"
 )
 func main(){
+	logger := configs.GetLogger("main")
+	//Initialize configs
+	err := configs.Init()
+	if err != nil {
+		logger.Errorf("Error on initialize configs: %v ",err)
+		panic(err)
+	}
 	router.InitRouter()
 }
---
+++ configs/configs.go
@@ -0,0 +1,19 @@
+package configs
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+	return nil
+}
+
+func GetLogger(prefix string) *Logger{
+	logger = NewLogger(prefix)
+	return logger
+}

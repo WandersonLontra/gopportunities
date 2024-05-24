3e3508b48eb121747fbf6ddfcb006fa75da9d1da
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2024-05-24 10:57:51 -0300

    chore: schema response

--- internal/schema/opening.go
+++ internal/schema/opening.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -12,4 +14,17 @@ type Opening struct {
 	Remote		bool
 	Link		string
 	Salary		float64
+}
+
+type OpeningResponse struct {
+	ID			uint		`json:"id"`
+	Role 		string		`json:"role"`
+	Company		string		`json:"company"`
+	Location	string		`json:"location"`
+	Remote		bool		`json:"remote"`
+	Link		string		`json:"link"`
+	Salary		float64		`json:"salary"`
+	CreatedAt	time.Time	`json:"createdAt"`
+	UpdatedAt	time.Time	`json:"updatedAt"`
+	DeletedAt	time.Time	`json:"deletedAt,omitempty"`
 }

995780f8d66333e59a9b5b884d4beb3ad76a353f
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2024-05-26 16:24:59 -0300

    feat: create get operation handler

--- internal/handler/getOpeningHandler.go
+++ internal/handler/getOpeningHandler.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/WandersonLontra/gopportunities/internal/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg":"get Opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, paramsRequiredHelper("id","string").Error())
+		return
+	}
+	var opening schema.Opening
+
+	// Find opening
+	if err := db.First(&opening,id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found",id))
+		return
+	}
+
+	sendSuccess(ctx, "get-opening",opening)
 }

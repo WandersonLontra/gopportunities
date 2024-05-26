10a451ba21fa8baa241edcb1c10e95bd33d69716
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2024-05-26 16:21:18 -0300

    feat: create list operation with pagination handler

--- internal/handler/listOpeningHandler.go
+++ internal/handler/listOpeningHandler.go
@@ -1,13 +1,38 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/WandersonLontra/gopportunities/internal/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg":"List Opening",
-	})
+	var (
+		limit int = 2
+		offset int = 0
+	)
+	
+	queryLimit := ctx.Query("limit")
+	queryOffset := ctx.Query("offset")
+
+	if queryLimit != "" {
+		lmt, _:= strconv.Atoi(queryLimit)
+		limit = lmt
+	}
+	if queryOffset != "" {
+		off, _:= strconv.Atoi(queryOffset)
+		offset = off
+	}
+	
+	var openings []schema.Opening
+
+	if err := db.Limit(limit).Offset(offset).Find(&openings).Error; err != nil {
+		logger.Errorf("Error on list openings: %v",err)
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("error on list openings"))
+		return
+	}
+	sendSuccess(ctx, "list",openings)
 }

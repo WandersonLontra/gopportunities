6819ffda13169406792fcd7a93ca9e05a60e23a4
Wanderson Lontra <wandersonlontra@yahoo.com.br>
2024-05-26 15:43:55 -0300

    feat: create delete operation handler

--- internal/handler/deleteOpeningHandler.go
+++ internal/handler/deleteOpeningHandler.go
@@ -1,13 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/WandersonLontra/gopportunities/internal/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg":"Delete Opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, paramsRequiredHelper("id","string").Error())
+		return
+	}
+	var opening schema.Opening
+
+	// Find opening
+	if err := db.First(&opening,id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found",id))
+		return
+	}
+	// Delete opening
+	if err := db.Delete(&opening, id).Error; err != nil {
+		logger.Errorf("error on delete opening: %v",err)
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error on delete opening with id: %s",id))
+	}
+	sendSuccess(ctx, "deletion",opening)
 }
